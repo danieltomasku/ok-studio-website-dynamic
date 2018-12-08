9e1f25f94546511797e8263b399acbd10d87550f
Anton Legoo <azeven@gmail.com>
2018-12-08 09:12:13 -0800

    \#10 Added google analytics

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import router from "./router.js";
 import store from "./store.js";
+import VueAnalytics from 'vue-analytics';
 
 import PrismicVue from 'prismic-vue';
 import linkResolver from '../prismic/link-resolver';
@@ -11,6 +12,12 @@ Vue.use( PrismicVue, {
 	endpoint: window.prismic.endpoint,
 	linkResolver,
 	htmlSerializer
+});
+
+// Init Google Analytics
+Vue.use( VueAnalytics, {
+	id: 'UA-129305382-1',
+	router
 });
 
 Vue.config.productionTip = false;
