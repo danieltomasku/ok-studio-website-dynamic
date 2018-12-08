31f5010dd1093f77410c51a9c736247439dec022
Daniel Tomás Ku <danieltomasku@gmail.com>
2018-12-08 12:23:22 -0800

    Updates to About page structure and style

--- src/app/modules/about/about.vue
+++ src/app/modules/about/about.vue
@@ -1,31 +1,35 @@
 <template>
-    <div class="">
+    <div v-if="content" :style="{background: `linear-gradient( to bottom, ${content.bg_gradient_start}, ${content.bg_gradient_finish})`}">
         <main-header></main-header>
 
-        <div class="pad-5-v" v-if="content">
-            <div class="container">
-                <h1 class="col-12">{{ content.hero_text[0].text }}</h1>
+        <div class="about-hero">
+            <div class="hero-title">
+                {{ content.hero_text[0].text }}
+            </div>
+            <div class="about-contact roles">
                 <div
-                    class="col-12"
+                    class="role"
                     v-for="(item, index) in content.contact_info"
                     :key="index + '-contact'"
                 >
-                    <strong v-if="item.type == 'heading2'">
+                    <div class="role-title" v-if="item.type == 'heading2'">
                         {{ item.text }}
-                    </strong>
-                    <span v-if="item.type == 'paragraph'">
+                    </div>
+                    <div class="role-item" v-if="item.type == 'paragraph'">
                         {{ item.text }}
-                    </span>
+                    </div>
                 </div>
-
-                <component  class="margin-4-v"
-                            v-for="(item, index) in content.body"
-                            :is="componentTypeForContent( item )"
-                            :content="item"
-                            :key="index">
-                </component>
-
             </div>
+            <!-- <img class="about-shape" src="images/about-shape.png"/> -->
+        </div>
+
+        <div class="pad-5-v" v-if="content">
+            <component  class="margin-4-v"
+                        v-for="(item, index) in content.body"
+                        :is="componentTypeForContent( item )"
+                        :content="item"
+                        :key="index">
+            </component>
         </div>
 
         <main-footer></main-footer>
@@ -114,5 +118,33 @@ export default
 //  ...
 ///////////////////////////////////////////////////////////
 
+header {
+    mix-blend-mode: unset;
+}
+
+.menu-button {
+    mix-blend-mode: unset;
+}
+
+.about-hero {
+    position: relative;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100%;
+    height: 100%;
+    transition: opacity 1s;
+    background-color: transparent;
+    padding: 30vh 100px 0;
+}
+
+.hero-title {
+    font-size: 5.2vw;
+    font-family: $font-family-base;
+    font-weight: 900;
+    color: white;
+    position: relative;
+    padding: 0 17% 55px 0;
+}
 
 </style>
