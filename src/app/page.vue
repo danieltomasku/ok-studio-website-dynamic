d5177e20e8ce67221a25a0ca019bae1a0dd61c41
Anton Legoo <azeven@gmail.com>
2018-12-13 21:33:30 -0800

    #9 added page transitions

--- src/app/page.vue
+++ src/app/page.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="">
     	<main-header></main-header>
-    	<router-view></router-view>
+    	<div ref="loading" class="loading-screen">
+    		<div ref="loading_name" class="loading-project-name">{{ pageTitle }}</div>
+			<svg ref="loading_logo" class="loading-image" width="80" viewBox="0 0 478 271" xmlns="http://www.w3.org/2000/svg"><g fill-rule="nonzero"><path d="M135.95 270.92c-74.41 0-135-60.54-135-135C.95 61.46 61.49.97 135.95.97c74.46 0 135 60.54 135 135 0 74.46-60.59 134.95-135 134.95zm0-238.92c-57.437614 0-104 46.562386-104 104s46.562386 104 104 104 104-46.562386 104-104c-.044102-57.4225744-46.577435-103.9648559-104-104.02V32zM365.33 135.95L475.59 270.9h-37.36l-112.3-134.95L438.23 1h39.06z"/></g></svg>
+		</div>
+    	<router-view ref="outlet"></router-view>
     	<main-footer></main-footer>
 	</div>
 </template>
@@ -46,7 +50,8 @@ export default
 					{ "property" : "og:locale", "content" : "en_US" },
 				],
 				"link" : [],
-			}
+			},
+			"loadingInt" : null,
 		}
 	},
 	
@@ -58,7 +63,19 @@ export default
 		{
 			// Assign if new value is not null.. if it is null, then use the default
 			this.head.title = (value && value.length > 0) ? value : this.head.titleDefault;
-		}
+		},
+
+		'$route' (to, from) 
+		{ 
+			// Determine if loading home
+			let isHome = to.path === "/";
+
+			// Clear any loading timeout
+			if( this.loadingInt ) window.clearTimeout( this.loadingInt );
+
+			// Show the loading screen
+			this.showLoading( isHome );
+		},
 	},
 
 	///////////////////////////////////////////////////////
@@ -69,6 +86,11 @@ export default
 	{
 		// Load content from prismic
         this.getMeta();
+
+        // Determine if loading home
+        let isHome = this.$route.path === "/";
+        // Show loading screen
+        this.showLoading( isHome );
 	},
 
 	"destroyed": function(){},
@@ -117,6 +139,25 @@ export default
         	this.head.link.push( { "rel": 'icon', 					"href" : prismicMetaObject.favicon.url, "type" : "image/png" } );
         	this.head.link.push( { "rel": 'shortcut icon', 			"href" : prismicMetaObject.favicon.url, "type" : "image/png" } );
         },
+
+		showLoading( isHome )
+		{
+			// If home, only show logo. If not, only show title
+			this.$refs.loading_logo.style.display = (isHome) ? 'block' : 'none';
+			this.$refs.loading_name.style.display = (isHome) ? 'none' : 'block';
+
+			// Show the loading screen
+			this.$refs.loading.classList.add("-shown");
+
+			// Hide after delay
+			this.loadingInt = window.setTimeout( this.hideLoading, 2000 );
+		},
+
+		hideLoading ()
+		{
+			// Hide the loading screen
+			this.$refs.loading.classList.remove("-shown");
+		},
 	},
 
 	"computed" : 
@@ -135,8 +176,78 @@ export default
 // 	...
 ///////////////////////////////////////////////////////////
 
-.template
-{}
+@keyframes colorBlend 
+{
+	0% 		{ background-color: #143e3b; }
+	14% 	{ background-color: #d6a49a; }
+	28% 	{ background-color: #dddfe2; }
+	42% 	{ background-color: #282626; }
+	57% 	{ background-color: #fbaf23; }
+	71% 	{ background-color: #bd401b; }
+	100% 	{ background-color: #143e3b; }
+}
+
+.loading-screen 
+{
+	visibility: hidden;
+	opacity: 0;
+
+	position: fixed;
+	z-index: 200;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+
+	animation: colorBlend 6s infinite;
+	animation-direction: alternate;
+	
+	$time : 1.5s;
+	transition: opacity $time,
+				visibility $time;
+
+	cursor: none;
+	font-family: GTWalsheim-Light, sans-serif;
+
+	&.-shown
+	{
+		visibility: visible;
+		opacity: 1;
+
+		transition: opacity 0s, visibility 0s;
+	}
+}
+
+.loading-project-name 
+{
+	position: absolute;
+	display: block;
+	transform: translate(-50%, -50%);
+	top: 50%;
+	left: 50%;
+	font-size: 70px;
+	color: white;
+	width: 80vw;
+	text-align: center;
+
+	@include breakpoint( $bp-mobile )
+	{
+		font-size: 40px;
+	}
+}
+
+.loading-screen svg 
+{
+	fill: #fff;
+}
+
+.loading-image 
+{
+	position: absolute;
+	transform: translate(-50%, -50%);
+	top: 50%;
+	left: 50%;
+}
 
 
 </style>
