339a49a3d7c44c0bc0813294a0ce47c1787ebc19
Anton Legoo <azeven@gmail.com>
2019-01-17 17:41:53 -0800

    #27 addresses flash of white on navbar when scrolling back to top

--- src/app/modules/shared/components/main-header.vue
+++ src/app/modules/shared/components/main-header.vue
@@ -2,11 +2,14 @@
 	<div class="wrapper">
 		<!-- Logo -->
 		<header ref="header">
-			<div class="logo">
-				<router-link to="/">
-					<svg width="50" viewBox="0 0 478 271" xmlns="http://www.w3.org/2000/svg"><g fill-rule="nonzero"><path d="M135.95 270.92c-74.41 0-135-60.54-135-135C.95 61.46 61.49.97 135.95.97c74.46 0 135 60.54 135 135 0 74.46-60.59 134.95-135 134.95zm0-238.92c-57.437614 0-104 46.562386-104 104s46.562386 104 104 104 104-46.562386 104-104c-.044102-57.4225744-46.577435-103.9648559-104-104.02V32zM365.33 135.95L475.59 270.9h-37.36l-112.3-134.95L438.23 1h39.06z"/></g></svg>
-				</router-link>
+			<div class="header-wrapper">
+				<div class="logo">
+					<router-link to="/">
+						<svg width="50" viewBox="0 0 478 271" xmlns="http://www.w3.org/2000/svg"><g fill-rule="nonzero"><path d="M135.95 270.92c-74.41 0-135-60.54-135-135C.95 61.46 61.49.97 135.95.97c74.46 0 135 60.54 135 135 0 74.46-60.59 134.95-135 134.95zm0-238.92c-57.437614 0-104 46.562386-104 104s46.562386 104 104 104 104-46.562386 104-104c-.044102-57.4225744-46.577435-103.9648559-104-104.02V32zM365.33 135.95L475.59 270.9h-37.36l-112.3-134.95L438.23 1h39.06z"/></g></svg>
+					</router-link>
+				</div>
 			</div>
+			<div class="white-bar"></div>
 		</header>
 
 		<!-- Menu Button -->
@@ -82,6 +85,9 @@ export default
 // 	...
 ///////////////////////////////////////////////////////////
 
+$nav-animation-timing: ease-out;
+$nav-animation-duration: 0.35s;
+
 header {
 	font-family: $font-family-base;
 	font-weight: bold;
@@ -92,39 +98,49 @@ header {
 	position: fixed;
 	top: 0;
 	z-index: 101;
-	padding: 0 50px;
-	mix-blend-mode: difference;
+	
 
-	@media (max-width: $bp-size-md) {
-		padding: 0 25px;
-	}
-
-	transition-timing-function: ease-out;
-	transition-duration: 0.25s;
+	transition-timing-function: $nav-animation-timing;
+	transition-duration: $nav-animation-duration;
 	transition-property: height, background, mix-blend-mode;
 
 	&.headroom--not-top
 	{
 		height: 70px;
-		background: white;
-		mix-blend-mode: normal;
 
 		.logo 
 		{
 			transform: translateY(-50%);
 			transition-property: transform;
 
-			svg { fill: black; }
+			// svg { fill: black; }
 		}
 
 		& + .menu-button
 		{
 			top: 24px;
 		}
+
+		.white-bar
+		{
+			opacity: 1;
+		}
+
 	}
 }
 
-.logo {
+.header-wrapper
+{
+	padding: 0 50px;
+	mix-blend-mode: difference;
+
+	@media (max-width: $bp-size-md) {
+		padding: 0 25px;
+	}
+}
+
+.logo 
+{
 	position: absolute;
 	display: inline-block;
 	transform: translateY(-36%);
@@ -133,6 +149,21 @@ header {
 	svg {
 		fill: white;
 	}
+}
+
+.white-bar
+{
+	position: absolute;
+	z-index: -1;
+
+	
+	width: 100%;
+	height: 100%;
+
+	background: white;
+	
+	opacity: 0;
+	transition: opacity $nav-animation-timing $nav-animation-duration;
 }
 
 .menu-button {
