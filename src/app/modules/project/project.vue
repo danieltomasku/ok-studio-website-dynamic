7c6e3003b4653d58bc7438b490a66b2394e6335b
Daniel Tomás Ku <danieltomasku@gmail.com>
2019-01-20 13:06:12 -0800

    Removed unused field call to cursor hover

--- src/app/modules/project/project.vue
+++ src/app/modules/project/project.vue
@@ -91,7 +91,7 @@ export default
         getContent ()
         {
             // Get article
-            this.$prismic.client.getByUID('project', this.$props.id, { 'fetchLinks': ['project.project_title', 'project.carousel_image', 'project.carousel_cursor_hover'] } )
+            this.$prismic.client.getByUID('project', this.$props.id, { 'fetchLinks': ['project.project_title', 'project.carousel_image'] } )
             // Handle article
             .then( (response, error) =>
             {
